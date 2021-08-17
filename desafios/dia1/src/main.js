4df0cb4b9e763b6522f8d44176c720c41ebb4742
joaooliveiradev <joaooliveira1775@gmail.com>
2021-08-17 11:33:19 -0300

    resolvendo o exercicio 4

--- desafios/dia1/src/main.js
+++ desafios/dia1/src/main.js
@@ -1,6 +1,18 @@
 import "./style.css";
 
-document.querySelector('[data-js="app"]').innerHTML = `
+const appElement = document.querySelector('[data-js="app"]');
+appElement.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `;
+const anchor = document.querySelector('[data-target="anchor"]');
+const changeVisibility = () => {
+  if (appElement.classList.contains("show")) {
+    appElement.classList.add("hide");
+    appElement.classList.remove("show");
+  } else {
+    appElement.classList.add("show");
+    appElement.classList.remove("hide");
+  }
+};
+anchor.addEventListener("click", changeVisibility);
