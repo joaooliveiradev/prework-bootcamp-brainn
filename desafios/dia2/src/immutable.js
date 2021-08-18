0710a2c3d1c65b1e29d859feff9483114c7acc34
joaooliveiradev <joaooliveira1775@gmail.com>
2021-08-18 08:56:41 -0300

    Desafio dia 2 exercicio 1 resolvido

--- desafios/dia2/src/immutable.js
+++ desafios/dia2/src/immutable.js
@@ -1,14 +1,15 @@
 const john = {
-  name: 'John',
-  surname: 'Doe',
+  name: "John",
+  surname: "Doe",
   age: 30,
-  hobbies: ['Surf', 'Design'],
-}
+  hobbies: ["Surf", "Design"],
+};
 
-const jane = john
+const jane = {
+  ...john,
+  name: "Jane",
+  hobbies: ["MuayThai", "Programming"],
+};
 
-jane.name = 'Jane'
-jane.hobbies.push('MuayThai', 'Programming')
-
-console.log('John:', john)
-console.log('Jane:', jane)
+console.log("John:", john);
+console.log("Jane:", jane);
